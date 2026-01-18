1815496c5d669fb8977c578c68a98573919cb872
XU, Qi <xuqi020020@gmail.com>
2026-01-18 19:58:45 +0000

    Update settings.py

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
