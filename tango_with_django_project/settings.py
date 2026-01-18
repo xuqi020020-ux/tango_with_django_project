dbb4fb136f50fa270edfe3f4b2be06e15aa444bf
XU, Qi <xuqi020020@gmail.com>
2026-01-18 22:11:10 +0000

    Add media file settings for user uploads

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Media files (user uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
